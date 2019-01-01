d34c5152b9582870c3e0bc8308ad29f1fd912db9
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2019-01-01 13:20:19 -0200

    refatorando código de registro de novos comandos
    Aqui estou usando decorators para automatizar o registro de comandos.
    * O comando /help lista os comandos disponíveis.
    * O nome da função de cada comando deve ser cmd_<nome>, onde "nome" é a palavra chave do comando. Por exemplo cmd_eventos implementa o comando /eventos
    * A docstring da função é usada pelo comando /help para descrever o que cada comando faz.

--- ChuviscoBot.py
+++ ChuviscoBot.py
@@ -23,6 +23,7 @@
 #  
 #  
 
+import functools
 import sys
 
 from telegram.ext import CommandHandler, Updater
@@ -58,32 +59,45 @@ if len(sys.argv) != 2:
   sys.exit(-1)
 
 token = sys.argv[1]
+bot = telegram.Bot(token)
+updater = Updater(token)
+dispatcher = updater.dispatcher
 
+BOT_CMDS = dict()
+def bot_command(func):
+	"""Register a function as a Telegram Bot command."""
+	name = func.__name__.split("cmd_")[1]
+	print(f"Registering /{name} command.")
 
-def help_(bot, update):
-	print("help")
-	"""Display all avaiable commands when /help is issued."""
-	message = '/eventos - Lista as atividades da agenda do Garoa Hacker Clube.'
-	update.message.reply_text(message)
+	@functools.wraps(func)
+	def func_wrapper(*args, **kwargs):
+	  print(f"/{name}... ", end="")
+	  ret_val = func(*args, **kwargs)
+	  print("DONE")
+	  return ret_val
 
-def eventos(bot, update):
-	"""Lista eventos mediante comando /eventos"""
-	print ("eventos")
+	if name not in BOT_CMDS:
+	  dispatcher.add_handler(CommandHandler(name, func_wrapper))
+
+	BOT_CMDS[name] = func.__doc__
+	return func_wrapper
+
+@bot_command
+def cmd_help(bot, update):
+	"""Exibe os comandos disponíveis."""
+	cmd_docs = "\n".join([f"  <b>/{name}</b> - {doc}" for name, doc in BOT_CMDS.items()])
+	update.message.reply_text(f"Comandos disponíveis:\n{cmd_docs}", parse_mode="HTML")
+
+@bot_command
+def cmd_eventos(bot, update):
+	"""Lista as atividades da agenda do Garoa Hacker Clube."""
 	lista_de_eventos = "\n".join([f"  - {evento}" for evento in agenda])
 	print(lista_de_eventos)
 	bot.send_message(chat_id=update.message.chat_id,
 	                 parse_mode="HTML",
 	                 text=f"Esta é a lista completa dos eventos futuros:\n{lista_de_eventos}")
-    
 
-bot = telegram.Bot(token)
-updater = Updater(token)
-dispatcher = updater.dispatcher
-
-dispatcher.add_handler(CommandHandler('help', help_))
-dispatcher.add_handler(CommandHandler('eventos', eventos))
 
 # Start the bot
 updater.start_polling()
-
 updater.idle()
