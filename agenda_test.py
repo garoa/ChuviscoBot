78a26f5bad43fe33a19bd19a429346b5dfd01926
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2019-01-06 23:04:40 -0200

    mais testes de código para as funções replace_wikilinks e replace_external_links

--- agenda_test.py
+++ agenda_test.py
@@ -1,14 +1,24 @@
-def test_replace_wikilink():
+from pytest import mark
+
+@mark.parametrize("given,want",[
+  ("hoje tem [[Noite do Arduino]] no Garoa",
+   "hoje tem <a href='https://garoa.net.br/wiki/Noite do Arduino'>Noite do Arduino</a> no Garoa"),
+  ("[https://www.meetup.com/pt-BR/Garoa-Hacker-Clube/events/257587273/ Organização & Flush no Garoa]",
+   "[https://www.meetup.com/pt-BR/Garoa-Hacker-Clube/events/257587273/ Organização & Flush no Garoa]"),
+])
+def test_replace_wikilink(given, want):
   from agenda import replace_wikilink
-  given = "hoje tem [[Noite do Arduino]] no Garoa"
-  want = "hoje tem <a href='https://garoa.net.br/wiki/Noite do Arduino'>Noite do Arduino</a> no Garoa"
   got = replace_wikilink(given)
   assert want == got
 
 
-def test_replace_external_link():
+@mark.parametrize("given,want",[
+  ("[https://www.meetup.com/pt-BR/Garoa-Hacker-Clube/events/257587273/ Organização & Flush no Garoa]",
+   "<a href='https://www.meetup.com/pt-BR/Garoa-Hacker-Clube/events/257587273/'>Organização & Flush no Garoa</a>"),
+  ("hoje tem [[Noite do Arduino]] no Garoa",
+   "hoje tem [[Noite do Arduino]] no Garoa")
+])
+def test_replace_external_link(given, want):
   from agenda import replace_external_link
-  given = "[https://www.meetup.com/pt-BR/Garoa-Hacker-Clube/events/257587273/ Organização & Flush no Garoa]"
-  want = "<a href='https://www.meetup.com/pt-BR/Garoa-Hacker-Clube/events/257587273/'>Organização & Flush no Garoa</a>"
   got = replace_external_link(given)
   assert want == got
