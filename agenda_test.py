01a18afc3cb1c704cb3d15264dc4bfa674c03899
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2019-01-06 23:07:03 -0200

    testes para a função replace_links

--- agenda_test.py
+++ agenda_test.py
@@ -22,3 +22,15 @@ def test_replace_external_link(given, want):
   from agenda import replace_external_link
   got = replace_external_link(given)
   assert want == got
+
+
+@mark.parametrize("given,want",[
+  ("hoje tem [[Noite do Arduino]] no Garoa",
+   "hoje tem <a href='https://garoa.net.br/wiki/Noite do Arduino'>Noite do Arduino</a> no Garoa"),
+  ("[https://www.meetup.com/pt-BR/Garoa-Hacker-Clube/events/257587273/ Organização & Flush no Garoa]",
+   "<a href='https://www.meetup.com/pt-BR/Garoa-Hacker-Clube/events/257587273/'>Organização & Flush no Garoa</a>"),
+])
+def test_replace_links(given, want):
+  from agenda import replace_links
+  got = replace_links(given)
+  assert want == got
